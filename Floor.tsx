00a6b1665644164b8957ca6b096c2684cc8ee40c
Fabs37 <75495468+Fabs37@users.noreply.github.com>
2021-02-22 19:49:39 +0000

    Deploying to gh-pages from master @ b4afca815223e2880274f93226cf317846622010 🚀

---
+++ Floor.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="Floor" tilewidth="32" tileheight="32" tilecount="128" columns="8">
+ <image source="../Tilesets/Modern_ Study Time (32x32)/Tile A5.png" width="256" height="512"/>
+</tileset>
